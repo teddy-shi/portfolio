9a3531d2d298e6aa69f8ef8978202091b4e8fda0
Teddy Shi <teddyshi@umich.edu>
2026-04-16 18:38:56 -0400

    Accessibility Checks

--- src/js/background.js
+++ src/js/background.js
@@ -5,21 +5,19 @@ const observerOptions = {
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
+        entry.target.setAttribute('aria-hidden', !entry.isIntersecting);
+
         if (entry.isIntersecting) {
             const id = entry.target.getAttribute('id');
-
+            
             if (entry.target.classList.contains('light-view')) {
                 document.body.classList.add('light-mode');
             } else {
                 document.body.classList.remove('light-mode');
             }
 
-            const navLinks = document.querySelectorAll('.footer-nav a');
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                if (link.getAttribute('href') === `#${id}`) {
-                    link.classList.add('active');
-                }
+            document.querySelectorAll('.footer-nav a').forEach(link => {
+                link.classList.toggle('active', link.getAttribute('href') === `#${id}`);
             });
         }
     });
